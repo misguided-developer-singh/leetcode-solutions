41f37ec48dff34070ec52a8e181a6ab92851f5fb
misguided-developer-singh <>
2024-12-20 14:23:04 +0530

    Plus One

---
+++ PlusOne.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class PlusOne {
+
+    public static int[] plusOne(int[] digits) {
+        for(int i = digits.length - 1; i >=0; i--) {
+            if(digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] newDigits = new int[digits.length + 1];
+        newDigits[0] = 1;
+        return newDigits;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {9, 9, 9};
+        System.out.println(Arrays.toString(plusOne(arr)));
+    }
+}
